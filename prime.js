3e0e8b4f82af1d4ddee5cdf5b56f301798b9a520
mohitfw24 <mohitsingh2443@gmail.com>
2022-12-10 10:46:16 +0530

    complete this task

---
+++ prime.js
@@ -0,0 +1,20 @@
+function checkPrime(num){
+let count=0;
+for(let i=0; i<=num; i++){
+if(num%i==0){
+count  ;
+}
+}
+if(count==2){
+return true;
+}
+return false;
+}
+let ans   = checkPrime(13);
+if(ans ==true){
+console.log("Prime")
+}
+  
+else{
+console.log("Not prime")
+}
